0573d50047b821b6ee40f687eabf0475adf41efa
Tristan Peralta <tristanperalta@protonmail.com>
2022-04-18 23:09:21 +0800

    Configure icons for autocomplete

--- config/nvim/lua/completion.lua
+++ config/nvim/lua/completion.lua
@@ -3,19 +3,66 @@ if not status_ok then
   return
 end
 
+local kind_icons = {
+  Text = "",
+  Method = "m",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+
 cmp.setup {
   snippet = {
     expand = function(args)
       vim.fn["vsnip#anonymous"](args.body)
     end
   },
-  preselect = cmp.PreselectMode.Item,
-  mappings = {
-    ["C-o"] = cmp.mapping.select_next_item()
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' }
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({ select = true })
   }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    { name = 'buffer' }
+  }),
+  view = {
+    entries = 'native'
+  },
+  formatting = {
+    fields = { 'kind', 'abbr', 'menu' },
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+      return vim_item
+    end
+  },
   experimental = {
     ghost_text = true
   }
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -59,6 +59,7 @@ return packer.startup(function(use)
 
   -- Autocompletion
   use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-vsnip'
   use 'hrsh7th/vim-vsnip'
