635a63c4331db2695aec13b20915607b1727d4ff
Tristan Peralta <tristanperalta@protonmail.com>
2022-04-19 00:30:41 +0800

    Key map for cmp.complete

--- config/nvim/lua/completion.lua
+++ config/nvim/lua/completion.lua
@@ -38,6 +38,7 @@ cmp.setup {
     end
   },
   mapping = cmp.mapping.preset.insert({
+    ['<C-space'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm({ select = true })
   }),
   sources = cmp.config.sources({
@@ -56,7 +57,7 @@ cmp.setup {
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
-        luasnip = "[Snippet]",
+        vsnip = "[Snippet]",
         buffer = "[Buffer]",
         path = "[Path]",
       })[entry.source.name]
