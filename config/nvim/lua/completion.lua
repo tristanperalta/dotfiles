a9d940cf9c4d5e9ebd67136e7c617e3acc94de8a
Tristan Peralta <tristanperalta@protonmail.com>
2022-04-19 20:35:22 +0800

    Super-tab mappings for nvim-cmp

--- config/nvim/lua/completion.lua
+++ config/nvim/lua/completion.lua
@@ -8,11 +8,6 @@ if not status_ok then
   return
 end
 
-local check_backspace = function()
-  local col = vim.fn.col "." - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-end
-
 local kind_icons = {
   Text = "",
   Method = "m",
@@ -41,6 +36,11 @@ local kind_icons = {
   TypeParameter = "",
 }
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -50,23 +50,18 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert({
     ['<C-Space'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expandable() then
-        luasnip.expand()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
-      elseif check_backspace() then
-        fallback()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
@@ -74,10 +69,7 @@ cmp.setup {
       else
         fallback()
       end
-    end, {
-      "i",
-      "s",
-    }),
+    end, { 'i' , 's' })
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
