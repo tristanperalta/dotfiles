6c6e9a294f644205b733c5babdd270681e4a85db
Tristan Peralta <tristanperalta@protonmail.com>
2023-07-13 17:13:53 +0800

    Selected languages to install on treesitter

--- config/nvim/lua/treesitter.lua
+++ config/nvim/lua/treesitter.lua
@@ -4,8 +4,8 @@ if not status_ok then
 end
 
 treesitter.setup {
-  ensure_installed = "all",
-  sync_install = true,
+  ensure_installed = {"c", "lua", "elixir", "css", "typescript", "bash", "eex", "rust", "javascript", "python"},
+  sync_install = false,
   autotag = { 
     enable = true,
     filetypes = { 'html', 'xml', 'heex', 'leex', 'eex' }
