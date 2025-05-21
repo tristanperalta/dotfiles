ce4595c4d0b3f5038333d6ea19d673d7060d17d0
Tristan Peralta <tristanperalta@protonmail.com>
2025-05-21 12:41:40 +0800

    Replace nvim-cmp with blink.cmp

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -103,25 +103,56 @@ return {
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = true
   },
-  {"hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-nvim-lsp" },
-    event = 'InsertEnter',
-    opts = function()
-      local cmp = require('cmp')
-      return {
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' }
-        })
-      }
-    end
+  {'saghen/blink.cmp',
+    -- use a release tag to download pre-built binaries
+    version = '1.*',
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = 'default' },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = { documentation = { auto_show = false } },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
   },
   {"mason-org/mason-lspconfig.nvim",
     dependencies = {
       {"mason-org/mason.nvim", opts = {}},
       "neovim/nvim-lspconfig"
     },
-    config = function()
-      -- for the list of LSP servers see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+    opts = function()
       local servers = {
         "elixirls",
         "ts_ls",
@@ -129,11 +160,10 @@ return {
         "pyright",
         "cssls",
       }
-
-      require('mason-lspconfig').setup({
+      return {
         ensure_installed = servers,
-        automatic_enable = servers
-      })
+        automatic_enable = servers,
+      }
     end
   }
 }
